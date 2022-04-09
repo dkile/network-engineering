0bc71cc564516b23f1b560b97082e4f50911449b
dkile <dkile532@naver.com>
2022-04-09 18:23:23 +0900

    function complete

--- hw2/easy_client/EasyTCPClient.go
+++ hw2/easy_client/EasyTCPClient.go
@@ -5,29 +5,195 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
 )
 
-func main() {
+type message struct {
+	Option  int    `json:"option"`
+	Content string `json:"content"`
+}
 
-    serverName := "nsl2.cau.ac.kr"
+func main() {
+		openSignalChannel("Bye bye~")
+
+    serverName := "localhost"
     serverPort := "33875"
 
-    conn, _:= net.Dial("tcp", serverName+":"+serverPort)
+    conn, err:= net.Dial("tcp", serverName+":"+serverPort)
+    if err != nil {
+        log.Fatalln("tcp connection error: ", err)
+    }
+
+    defer conn.Close()
 
     localAddr := conn.LocalAddr().(*net.TCPAddr)
     fmt.Printf("Client is running on port %d\n", localAddr.Port)
 
-    fmt.Printf("Input lowercase sentence: ")
-    input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-    conn.Write([]byte(input))
+    for {
 
-    buffer := make([]byte, 1024)
-    conn.Read(buffer)
-    fmt.Printf("Reply from server: %s", string(buffer))
+		// choose input option
+		var input_option string
+		fmt.Println("<Menu>")
+		fmt.Println("1) convert text to UPPER-case")
+		fmt.Println("2) get my IP address and port number")
+		fmt.Println("3) get server request count")
+		fmt.Println("4) get server running time")
+		fmt.Println("5) exit")
+		fmt.Printf("Input option: ")
+		_, err := fmt.Scanln(&input_option)
+		if err != nil {
+			log.Fatalln("scanning input error: ", err)
+		}
+		option, _ := strconv.Atoi(input_option)
 
-    conn.Close()
+		switch option {
+		case 1:
+			var input_sentence string
+			fmt.Printf("Input lowercase sentence: ")
+			fmt.Scanln(&input_sentence)
+
+			req_msg := createJsonMessage(1, input_sentence)
+
+			req_time := time.Now()
+
+			conn.Write(req_msg)
+
+			buffer := make([]byte, 1024)
+			count, err := conn.Read(buffer)
+
+			rtt := float64(time.Now().Sub(req_time)) / float64(time.Millisecond)
+
+			if err != nil {
+				if err == io.EOF {
+					log.Fatalln("server disconnected by timeout")
+				}
+				log.Fatalln("reading response error: ", err)
+			}
+
+			_, content := decodeJsonMessage(buffer[:count])
+
+			fmt.Println("Reply from server: ", content)
+			fmt.Println("RTT = ", rtt, "ms")
+		case 2:
+			req_msg := createJsonMessage(option, "")
+			req_time := time.Now()
+			conn.Write(req_msg)
+
+			buffer := make([]byte, 1024)
+			count, err := conn.Read(buffer)
+			rtt := float64(time.Now().Sub(req_time)) / float64(time.Millisecond)
+
+			if err != nil {
+				if err == io.EOF {
+					log.Fatalln("no response from server")
+				}
+				log.Fatalln("reading response error: ", err)
+			}
+
+			_, content := decodeJsonMessage(buffer[:count])
+
+			ip := strings.Split(content, ":")[0]
+			port := strings.Split(content, ":")[1]
+			fmt.Println("Reply from server: client IP=", ip, "port=", port)
+			fmt.Println("RTT = ", rtt, "ms")
+		case 3:
+			req_msg := createJsonMessage(option, "")
+
+			req_time := time.Now()
+
+			conn.Write(req_msg)
+
+			buffer := make([]byte, 1024)
+			count, err := conn.Read(buffer)
+
+			rtt := float64(time.Now().Sub(req_time)) / float64(time.Millisecond)
+
+			if err != nil {
+				if err == io.EOF {
+					log.Fatalln("no response from server")
+				}
+				log.Fatalln("reading response error: ", err)
+			}
+
+			_, content := decodeJsonMessage(buffer[:count])
+			fmt.Println("Reply from server: requests served=", content)
+			fmt.Println("RTT = ", rtt, "ms")
+		case 4:
+			req_msg := createJsonMessage(option, "")
+
+			req_time := time.Now()
+
+			conn.Write(req_msg)
+
+			buffer := make([]byte, 1024)
+			count, err := conn.Read(buffer)
+
+			rtt := float64(time.Now().Sub(req_time)) / float64(time.Millisecond)
+
+			if err != nil {
+				if err == io.EOF {
+					log.Fatalln("no response from server")
+				}
+				log.Fatalln("reading response error: ", err)
+			}
+
+			_, content := decodeJsonMessage(buffer[:count])
+			fmt.Println("Reply from server: runtime=", content)
+			fmt.Println("RTT = ", rtt, "ms")
+		case 5:
+			conn.Close()
+			fmt.Println("Bye bye~")
+			os.Exit(0)
+		default:
+			fmt.Println("Please enter number between 1 and 5")
+		}
+	}
+}
+
+func openSignalChannel(ment string) {
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGKILL, syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
+
+	go func() {
+		sig := <-sigs
+		if sig.String() == "interrupt" {
+			fmt.Println()
+			fmt.Println(ment)
+		} else {
+			fmt.Println("unhandled signal")
+		}
+		close(sigs)
+		os.Exit(0)
+	}()
+}
+
+func createJsonMessage(opt int, content string) []byte {
+	req_msg := message{Option: opt, Content: content}
+	// encode struct to json byte array
+	json_msg, err := json.Marshal(req_msg)
+	if err != nil {
+		log.Fatalln("json encode error: ", err)
+	}
+	return json_msg
+}
+
+/**
+*
+ */
+func decodeJsonMessage(buffer []byte) (int, string) {
+	var res_msg message
+	json.Unmarshal(buffer, &res_msg)
+
+	return res_msg.Option, res_msg.Content
 }
--- hw2/easy_server/EasyTCPServer.go
+++ hw2/easy_server/EasyTCPServer.go
@@ -5,25 +5,150 @@
 package main
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
 )
 
-func main() {
-    serverPort := "33875"
-
-    listener, _:= net.Listen("tcp", ":" + serverPort)
-    fmt.Printf("Server is ready to receive on port %s\n", serverPort)
-
-    buffer := make([]byte, 1024)
-
-    for {
-        conn, _:= listener.Accept()
-        fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
-        count, _ := conn.Read(buffer)
-        conn.Write(bytes.ToUpper(buffer[:count]))
-        conn.Close()
-    }
+type message struct {
+	Option  int    `json:"option"`
+	Content string `json:"content"`
 }
 
+func main() {
+
+	openSignalChannel("Bye bye~")
+
+	start_time := time.Now()
+	serverPort := "33875"
+
+	listener, _ := net.Listen("tcp", ":"+serverPort)
+	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
+
+	defer listener.Close()
+
+	req_cnt := 0
+
+	for {
+		conn, err := listener.Accept()
+		time_ch := make(chan time.Time)
+		if err != nil {
+			log.Println("connection error: ", err)
+		}
+
+		go func() {
+			buffer := make([]byte, 4096)
+			for {
+				count, err := conn.Read(buffer)
+				if err != nil {
+					if err == io.EOF {
+							log.Println("connection closed from client ", conn.RemoteAddr().String())
+						} else {
+							log.Println("Read error: ", err)
+						}
+						time_ch <- time.Time{}
+						return
+					}
+				time_ch <- time.Now()
+				fmt.Println("connect with ", conn.RemoteAddr().String())
+
+				req_cnt += 1
+
+				var req_msg message
+				json.Unmarshal(buffer[:count], &req_msg)
+
+				switch req_msg.Option {
+				case 1:
+					upper_sentence := strings.ToUpper(req_msg.Content)
+					res_msg := message{Option: 1, Content: upper_sentence}
+					json_msg, _ := json.Marshal(res_msg)
+					conn.Write(json_msg)
+					fmt.Println("case 1")
+				case 2:
+					res_msg := message{Option: 2, Content: conn.RemoteAddr().String()}
+					json_msg, _ := json.Marshal(res_msg)
+					conn.Write(json_msg)
+					fmt.Println("case 2")
+				case 3:
+					res_msg := message{Option: 3, Content: strconv.Itoa(req_cnt)}
+					json_msg, _ := json.Marshal(res_msg)
+					conn.Write(json_msg)
+					fmt.Println("case 3")
+				case 4:
+					duration := time.Now().Sub(start_time)
+					res_msg := message{Option: 4, Content: fmtDuration(duration)}
+					json_msg, _ := json.Marshal(res_msg)
+					conn.Write(json_msg)
+					fmt.Println("case 4")
+				default:
+					conn.Write([]byte("error"))
+					fmt.Println("case 5")
+				}
+			}
+		}()
+
+		go func() {
+			timer := time.NewTimer(time.Second * 30)
+			fmt.Println(conn, "timer started")
+			for {
+				select {
+				case req_time:= <- time_ch:
+					if req_time.IsZero() {
+						// Use SetLinger to force close the connection
+						err := conn.(*net.TCPConn).SetLinger(0)
+						if err != nil {
+							log.Printf("Error when setting linger: %s", err)
+						}
+						fmt.Println("connection ", conn, " close")
+						conn.Close()
+						return
+					} else {
+						timer.Reset(time.Second * 30)
+						fmt.Println(conn, "timer reset")
+					}
+				case <- timer.C:
+					fmt.Println(conn, "timer end")
+					fmt.Println("conenction", conn, "close")
+					conn.Close()
+					return
+				}
+		}
+		}()
+	}
+}
+
+func openSignalChannel(ment string) {
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGKILL, syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
+
+	go func() {
+		sig := <-sigs
+		if sig.String() == "interrupt" {
+			fmt.Println()
+			fmt.Println(ment)
+			close(sigs)
+			os.Exit(0)
+		} else {
+			fmt.Println("unhandled signal")
+		}
+	}()
+}
+
+func fmtDuration(d time.Duration) string {
+	d = d.Round(time.Second)
+	h := d / time.Hour
+	d -= h * time.Hour
+	m := d / time.Minute
+	d -= m * time.Minute
+	s := d / time.Second
+	return fmt.Sprintf("%02d:%02d:%02d", h, m, s)
+}
